b72a7c922d04465ce80231598cc98ab5b6545006
unknown <rikardoan@gmail.com>
2021-12-26 18:44:25 -0500

    fixed new post

---
+++ public/js/new-post.js
@@ -0,0 +1,53 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector("#post-name").value.trim();
+  // const needed_funding = document
+  //   .querySelector("#project-funding")
+  //   .value.trim();
+  const content = document.querySelector("#post-desc").value.trim();
+  alert("titlw" + title + content);
+  if (title && content) {
+    const response = await fetch(`/api/posts`, {
+      method: "POST",
+      body: JSON.stringify({ title, content }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert("Failed to create project");
+    }
+  }
+};
+
+// const delButtonHandler = async (event) => {
+//   if (event.target.hasAttribute("data-id")) {
+//     const id = event.target.getAttribute("data-id");
+
+//     const response = await fetch(`/api/posts/${id}`, {
+//       method: "DELETE",
+//     });
+
+//     if (response.ok) {
+//       document.location.replace("/profile");
+//     } else {
+//       alert("Failed to delete project");
+//     }
+//   }
+// };
+
+document
+  .querySelector(".new-project-form")
+  .addEventListener("click", newFormHandler);
+
+// document
+//   .querySelector(".project-list")
+//   .addEventListener("click", delButtonHandler);
+
+// document
+//   .querySelector(".project-list")
+//   .addEventListener("click", newFormHandler);
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,66 +1,33 @@
-const newFormHandler = async (event) => {
-  event.preventDefault();
+// const delButtonHandler = async (event) => {
+//   if (event.target.hasAttribute("data-id")) {
+//     const id = event.target.getAttribute("data-id");
 
-  const title = document.querySelector("#project-name").value.trim();
-  // const needed_funding = document
-  //   .querySelector("#project-funding")
-  //   .value.trim();
-  const content = document.querySelector("#project-desc").value.trim();
+//     const response = await fetch(`/api/projects/${id}`, {
+//       method: "DELETE",
+//     });
 
-  if (title && content) {
-    const response = await fetch(`/api/projects`, {
-      method: "POST",
-      body: JSON.stringify({ title, content }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+//     if (response.ok) {
+//       document.location.replace("/profile");
+//     } else {
+//       alert("Failed to delete project");
+//     }
+//   }
+// };
 
-    if (response.ok) {
-      document.location.replace("/profile");
-    } else {
-      alert("Failed to create project");
-    }
-  }
-};
+// const edtButtonHandler = async (event) => {
+//   if (event.target.hasAttribute("data-id")) {
+//     const id = event.target.getAttribute("data-id");
 
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute("data-id")) {
-    const id = event.target.getAttribute("data-id");
+//     const response = await fetch(`/api/projects/${id}`, {
+//       method: "UPDATE",
+//     });
 
-    const response = await fetch(`/api/projects/${id}`, {
-      method: "DELETE",
-    });
-
-    if (response.ok) {
-      document.location.replace("/profile");
-    } else {
-      alert("Failed to delete project");
-    }
-  }
-};
-//*--edtButtonHandler
-const edtButtonHandler = async (event) => {
-  if (event.target.hasAttribute("data-id")) {
-    const id = event.target.getAttribute("data-id");
-
-    const response = await fetch(`/api/projects/${id}`, {
-      method: "UPDATE",
-    });
-
-    if (response.ok) {
-      document.location.replace("/profile");
-    } else {
-      alert("Failed to update project");
-    }
-  }
-};
-//-----**---
-
-document
-  .querySelector(".new-project-form")
-  .addEventListener("submit", newFormHandler);
-
-document.querySelector(".delete").addEventListener("click", delButtonHandler);
+//     if (response.ok) {
+//       document.location.replace("/profile");
+//     } else {
+//       alert("Failed to update project");
+//     }
+//   }
+// };
 
 // document.querySelector(".edit").addEventListener("click", edtButtonHandler);
