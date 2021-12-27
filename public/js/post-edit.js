8b13ad0dbb272e3b8945f01805a9724b143e15a2
unknown <rikardoan@gmail.com>
2021-12-26 22:55:31 -0500

    bootstrap dashbboard page

---
+++ public/js/post-edit.js
@@ -0,0 +1,61 @@
+const newFormHandler = async (event) => {};
+
+const editButtonHandler = async (event) => {
+  const title = document.querySelector("#title").value.trim();
+  const id = document.querySelector("#postid").value.trim();
+
+  const content = document.querySelector("#post-desc").value.trim();
+  console.log(title);
+  console.log(id);
+  console.log(content);
+  alert(title + "Edited");
+  if (title && content) {
+    const response = await fetch(`/api/posts/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({ title, content }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to update project");
+    }
+  }
+};
+const delFormHandler = async (event) => {};
+
+const delButtonHandler = async (event) => {
+  const title = document.querySelector("#title").value.trim();
+  const id = document.querySelector("#postid").value.trim();
+
+  const content = document.querySelector("#post-desc").value.trim();
+  console.log(title);
+  console.log(id);
+  console.log(content);
+  alert("Post Deleted");
+  if (title && content) {
+    const response = await fetch(`/api/posts/${id}`, {
+      method: "DELETE",
+      body: JSON.stringify({ title, content }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to delete project");
+    }
+  }
+};
+
+document
+  .querySelector(".project-list")
+  .addEventListener("click", editButtonHandler);
+document
+  .querySelector(".delete-post")
+  .addEventListener("click", delButtonHandler);
